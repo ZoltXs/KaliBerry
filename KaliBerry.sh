ab7fe325db1c8d5613e4f0de67541d56f057ebf7
ZoltXs <Jartada44@hotmail.com>
2025-06-01 16:47:35 +0200

    Update KaliBerry.sh

--- KaliBerry.sh
+++ KaliBerry.sh
@@ -300,8 +300,8 @@ colorberrykbd() {
         return 1
     }
     # Command 1
-    show_progress "cd KaliBerry" "Cambiando al directorio KaliBerry"
-    sleep 1    
+    # show_progress "cd KaliBerry" "Cambiando al directorio KaliBerry"
+    # sleep 1    
     # Command 2
     show_progress "cd beepberry-keyboard-driver" "Cambiando al directorio del driver de teclado"
     sleep 1
